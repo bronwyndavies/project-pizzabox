ef53ca70a918c04b2c9acae85313e677407f1285
Bronwyn Davies <bldavies93@gmail.com>
2021-03-31 17:20:56 -0400

    initial setup

---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaBox.Client
+{
+  internal class Program //access modifiers - internal, protected, public, private 
+  {
+    private static void Main(string[] args)
+    {
+      Run();
+    }
+    private static void Run() //needs to be static...static vs constant vs readonly
+    {
+      System.Console.WriteLine("Welcome to PizzaBox");
+      var stores = new List<string>()
+      {
+          "Store 001",
+          "Store 002",
+          "Store 003"
+      };
+
+      foreach (var item in stores)
+      {
+        System.Console.WriteLine(item);
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Allows customer to order pizza, 
